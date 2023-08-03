d1179939d2dd97bdea3b5c5da02011648289d36d
riddhijain25 <88544980+riddhijain25@users.noreply.github.com>
2023-08-03 21:59:09 +0530

    Add files via upload

---
+++ demo/src/main/java/com/api/entity/Product.java
@@ -0,0 +1,54 @@
+package com.api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+
+@Entity
+public class Product {
+
+    @Id
+    private Integer productId;
+
+    private String productName;
+
+    private String productDescription;
+
+    private LocalDate expDate;
+
+    public void setProductId(Integer productId){
+        this.productId = productId;
+    }
+
+    public void setProductName(String ProductName){
+        this.productName = productName;
+    }
+    public void setProductDescription(String productDescription){
+        this.productDescription = productDescription;
+    }
+    public void setExpDate(LocalDate expDate){
+        this.expDate = expDate;
+    }
+
+    public Integer getProductId(){
+        return this.productId;
+    }
+
+    public String getProductName(){
+        return this.productName;
+    }
+
+    public String getProductDescription(){
+        return this.productDescription;
+    }
+
+    public LocalDate getExpDate(){
+        return this.expDate;
+    }
+    @Override
+    public String toString() {
+        return this.productName;
+    }
+}
